6869d5f9a754b11a6e31bc41d88535016de4619a
Chrystalla <c.fella@student.maastrichtuniversity.nl>
2025-01-07 00:41:08 +0100

    button show my work implementation

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,3 +1,5 @@
+import { FaLocationArrow } from 'react-icons/fa'
+import MagicButton from './ui/MagicButton'
 import { Spotlight } from './ui/Spotlight'
 import { TextGenerateEffect } from './ui/TextGenerateEffect'
 
@@ -23,11 +25,18 @@ const Hero = () => {
           </h2>
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Turning Innovations into Intuitive User Experiences."
           />
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi, I&apos;m Chrystalla, a Software Engineer based in Netherlands.
           </p>
+          <a href="#about">
+            <MagicButton 
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position='right'
+            />
+            </a>
         </div>
       </div>
     </div>
